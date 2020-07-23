5c0dfe890afad44ad51f3ec799a06129e31bb79d
HarindaVithana <harindavithana@gmail.com>
2020-07-23 15:38:36 +0530

    Update olresults.php

--- application/views/pages/olresults.php
+++ application/views/pages/olresults.php
@@ -1,199 +1,186 @@
-<h2><?= $title; ?></h2>
-<div class="container1" id="con1">
-  <h2>O/L Results Sheet</h2>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Subject Number</th>
-        <th>Subject Name</th>
-        <th>Grade</th>
-        <th>Medium</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>0001</td>
-        <td>abc</td>
-        <td>A</td>
-        <td>English</td>
-      </tr>      
-      <tr class="info">
-        <td>0002</td>
-        <td>def</td>
-        <td>A</td>
-        <td>English</td>
-      </tr>
-      <tr class="">
-        <td>Info</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-        <td>Active</td>
-      </tr>
-      <tr class="info">
-        <td>Warning</td>
-        <td>Refs</td>
-        <td>bo@example.com</td>
-        <td>Active</td>
-      </tr>
-      <tr>
-        <td>0001</td>
-        <td>abc</td>
-        <td>A</td>
-        <td>English</td>
-      </tr>      
-      <tr class="info">
-        <td>Active</td>
-        <td>Activeson</td>
-        <td>act@example.com</td>
-        <td>Active</td>
-      </tr>
-      <tr>
-        <td>0001</td>
-        <td>abc</td>
-        <td>A</td>
-        <td>English</td>
-      </tr>  
-      <tr class="info">
-        <td>Active</td>
-        <td>Activeson</td>
-        <td>act@example.com</td>
-        <td>Active</td>
-      </tr>    
-    </tbody>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>O/L Results</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<div class="container">
+<h2>O/L Results Sheet</h2><br>
+
+<div class="row">
+
+
+    <div class="container1" id="con1">    
+      <div class= "col-sm-8">
+      <h4>1st Attempt</h4><br>
+      <table class="table" style="max-width: 400px;">
+        <thead>
+          <tr>
+            <th>Subject Name</th>
+            <th>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>abc</td>
+            <td>A</td>
+          </tr>      
+          <tr class="info">
+            <td>def</td>
+            <td>A</td>
+          </tr>
+          <tr class="">
+            <td>Dooley</td>
+            <td>july@example.com</td>
+          </tr>
+          <tr class="info">
+            <td>Refs</td>
+            <td>bo@example.com</td>
+          </tr>
+          <tr>
+            <td>abc</td>
+            <td>A</td>
+          </tr>      
+          <tr class="info">
+            <td>Activeson</td>
+            <td>act@example.com</td>
+          </tr>
+          <tr>
+            <td>abc</td>
+            <td>A</td>
+          </tr>  
+          <tr class="info">
+            <td>Activeson</td>
+            <td>act@example.com</td>
+          </tr>  
+          <tr class="">
+            <td>Activeson</td>
+            <td>act@example.com</td>
+          </tr>    
+        </tbody>
+      </table>
+    </div>      
+    </div>
+
+
+    <div class="container2" id="con2">
+    <div class= "col-sm-8">
+    <h4>2nd Attempt</h4><br>
+      <table class="table"  style="max-width: 400px;">
+        <thead>
+          <tr>
+            <th>Subject Name</th>
+            <th>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>abc</td>
+            <td>A</td>
+          </tr>      
+          <tr class="info">
+            <td>def</td>
+            <td>A</td>
+          </tr>
+          <tr class="">
+            <td>Dooley</td>
+            <td>july@example.com</td>
+          </tr>
+          <tr class="info">
+            <td>Refs</td>
+            <td>bo@example.com</td>
+          </tr>
+          <tr>
+            <td>abc</td>
+            <td>A</td>
+          </tr>      
+          <tr class="info">
+            <td>Activeson</td>
+            <td>act@example.com</td>
+          </tr>
+          <tr>
+            <td>abc</td>
+            <td>A</td>
+          </tr>  
+          <tr class="info">
+            <td>Activeson</td>
+            <td>act@example.com</td>
+          </tr> 
+          <tr class="">
+            <td>Activeson</td>
+            <td>act@example.com</td>
+          </tr>     
+
+        </tbody>
+      </table>
+      </div>
+    </div>
+
+    <div class="container3" id="con3">
+      <div class= "col-sm-8">
+      <h4>3rd Attempt</h4><br>
+      <table class="table"  style="max-width: 400px;">
+        <thead>
+          <tr>
+            <th>Subject Name</th>
+            <th>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>abcdkvn</td>
+            <td>A</td>
+          </tr>      
+          <tr class="info">
+            <td>def</td>
+            <td>A</td>
+          </tr>
+          <tr class="">
+            <td>Dooley</td>
+            <td>july@example.com</td>
+          </tr>
+          <tr class="info">
+            <td>Refs</td>
+            <td>bo@example.com</td>
+          </tr>
+          <tr>
+            <td>abc</td>
+            <td>A</td>
+          </tr>      
+          <tr class="info">
+            <td>Activeson</td>
+            <td>act@example.com</td>
+          </tr>
+          <tr>
+            <td>abc</td>
+            <td>A</td>
+          </tr>  
+          <tr class="info">
+            <td>Activeson</td>
+            <td>act@example.com</td>
+          </tr>   
+          <tr class="">
+            <td>Activeson</td>
+            <td>act@example.com</td>
+          </tr>   
+
+        </tbody>
+      </table>
+      </div>
+    </div>
+
   </table>
+  </div>
+</div>
+
 </div>
 
 
-<div class="container2" id="con2">
-  <h2>O/L Results Sheet</h2>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Subject Number</th>
-        <th>Subject Name</th>
-        <th>Grade</th>
-        <th>Medium</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>0001</td>
-        <td>abc</td>
-        <td>A</td>
-        <td>English</td>
-      </tr>      
-      <tr class="info">
-        <td>0002</td>
-        <td>def</td>
-        <td>A</td>
-        <td>English</td>
-      </tr>
-      <tr class="">
-        <td>Info</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-        <td>Active</td>
-
-      </tr>
-      <tr class="info">
-        <td>Warning</td>
-        <td>Refs</td>
-        <td>bo@example.com</td>
-        <td>Active</td>
-      </tr>
-      <tr>
-        <td>0001</td>
-        <td>abc</td>
-        <td>A</td>
-        <td>English</td>
-      </tr>      
-      <tr class="info">
-        <td>Active</td>
-        <td>Activeson</td>
-        <td>act@example.com</td>
-        <td>Active</td>
-      </tr>
-      <tr>
-        <td>0001</td>
-        <td>abc</td>
-        <td>A</td>
-        <td>English</td>
-      </tr>  
-      <tr class="info">
-        <td>Active</td>
-        <td>Activeson</td>
-        <td>act@example.com</td>
-        <td>Active</td>
-      </tr>    
-
-    </tbody>
-  </table>
-</div>
-
-<div class="container3" id="con3">
-  <h2>O/L Results Sheet</h2>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Subject Number</th>
-        <th>Subject Name</th>
-        <th>Grade</th>
-        <th>Medium</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>0001</td>
-        <td>abc</td>
-        <td>A</td>
-        <td>English</td>
-      </tr>      
-      <tr class="info">
-        <td>0002</td>
-        <td>def</td>
-        <td>A</td>
-        <td>English</td>
-      </tr>
-      <tr class="">
-        <td>Info</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-        <td>Active</td>
-
-      </tr>
-      <tr class="info">
-        <td>Warning</td>
-        <td>Refs</td>
-        <td>bo@example.com</td>
-        <td>Active</td>
-      </tr>
-      <tr>
-        <td>0001</td>
-        <td>abc</td>
-        <td>A</td>
-        <td>English</td>
-      </tr>      
-      <tr class="info">
-        <td>Active</td>
-        <td>Activeson</td>
-        <td>act@example.com</td>
-        <td>Active</td>
-      </tr>
-      <tr>
-        <td>0001</td>
-        <td>abc</td>
-        <td>A</td>
-        <td>English</td>
-      </tr>  
-      <tr class="info">
-        <td>Active</td>
-        <td>Activeson</td>
-        <td>act@example.com</td>
-        <td>Active</td>
-      </tr>    
-    </tbody>
-  </table>
-</div>
-<script src="<?php echo site_url(); ?>assets/js/olResult.js"></script>
-
+</body>
+</html>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="<?php echo site_url(); ?>assets/js/ol.js"></script>
 
