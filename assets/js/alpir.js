8e27a0ec4eefac93f63c73b0e938b2222a79c716
HarindaVithana <harindavithana@gmail.com>
2020-08-13 13:45:39 +0530

    Update alpir.js

--- assets/js/alpir.js
+++ assets/js/alpir.js
@@ -2,7 +2,9 @@ $("#alyear").change(function(){
     var alyear = $(this).val();
 
     if(alyear == 2018){
+        $("#alyear")[0].selectedIndex = "";
         window.location.href = "http://localhost/ncoe/al";
+
     }
     // alert(alyear);
 
