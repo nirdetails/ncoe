8d9d89a926a3699f8f85704059514f5bda999f51
HarindaVithana <harindavithana@gmail.com>
2020-08-14 10:36:25 +0530

    Add about.txt

--- application/views/pages/prefer.php
+++ application/views/pages/prefer.php
@@ -34,21 +34,22 @@
 <?php } ?>
   <div class="row" >
     <div id="certificates" style="display :none">
-      <div class="col-sm-6">
+      <div class="col-sm-12">
         <div class="form-group">
           <label for="coursename"><b>Physical Education Marks: </b></label>
           <input type="number" class="form-control" id="pemarks1" name="pemarks1" >    
         </div>
       </div>
-      <div id="div2" class="col-sm-6">
-        <label for="myfile"  ><b>Upload your Physical Education Certificates:</b></label>
+      <div id="div2" class="col-sm-12">
+        <label for="myfile"  ><b>Upload your Physical Education Certificates:</b></label><br>
         <input title="you can upload 3 Certificates only" type="file" id="peupload1" name="peupload1" multiple><br><br>
         <input title="you can upload 3 Certificates only" type="file" id="peupload2" name="peupload2" multiple><br><br>
         <input title="you can upload 3 Certificates only" type="file" id="peupload3" name="peupload3" multiple><br><br>
+        <label><p><span style="color:red">* </span><i>Rename your files with NIC number "_" and file order<br><b>Ex: 90167236V_1.pdf</b></i></p></label>
       </div>
     </div>
   </div>
-  <div class="col-sm-6" id="category"  style="display :none">  
+  <div class="col-sm-12" id="category"  style="display :none">  
     <div class="form-group">
       <label for="category">Category: </label><br>
       <input type="radio" name="category" value="Catholic">Catholic
@@ -60,6 +61,7 @@
     <input title="you can upload 3 Certificates only" type="file" id="rupload1" name="rupload1" multiple><br><br>
     <input title="you can upload 3 Certificates only" type="file" id="rupload2" name="rupload2" multiple><br><br>
     <input title="you can upload 3 Certificates only" type="file" id="rupload3" name="rupload3" multiple><br><br>
+    <label><p><span style="color:red">* </span><i>Rename your files with NIC number "_" and file order<br><b>Ex: 90167236V_1.pdf</b></i></p></label>
   </div>
 
   <div id="west" style="display :none">
@@ -67,6 +69,7 @@
     <input title="you can upload 3 Certificates only" type="file" id="wupload1" name="wupload1" multiple><br><br>
     <input title="you can upload 3 Certificates only" type="file" id="wupload2" name="wupload2" multiple><br><br>
     <input title="you can upload 3 Certificates only" type="file" id="wupload3" name="wupload3" multiple><br><br>
+    <label><p><span style="color:red">* </span><i>Rename your files with NIC number "_" and file order<br><b>Ex: 90167236V_1.pdf</b></i></p></label>
   </div>
 
   <div style="text-align :right">     
