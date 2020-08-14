7947789fd2488646ca835583b52875fe5b96f005
HarindaVithana <harindavithana@gmail.com>
2020-08-14 14:49:09 +0530

    update declaration.php

--- application/views/pages/declaration.php
+++ application/views/pages/declaration.php
@@ -3,18 +3,35 @@
     <div class="form-group">
       <h2 style="text-align:center">Applicant's Declaration</h2>
     </div>
-<ol style="line-height:25px; text-align:justify">
+  <ol style="line-height:25px; text-align:justify">
     <li> I am aware that I have forwarded my application in accordance with paragraph10.0 of the Gazette Notification, after reading and comprehending the Gazette Notification well.</li>
     <li> I certify that I have already acquired the qualifications mentioned in the Gazette Notification relevant to the Courses that have been applied by me.</li>
     <li> I am aware that the particulars furnished in the application by me are true and correct. I am aware that if any particulars given by me in this application are found to be false or incorrect or contradictory to the Gazette Notification, I am liable to be disqualified before selection or to be discontinued after selection.</li>
     <li> I certify that I have not registered to follow a course in a University/University Institute/any other Higher Education Institute/ I am not currently following a relevant course in an afore mentioned institute. I further certify that I agree to be discontinued from the course if It is found that the section 8.4 of the Gazette Notification has been/is violated by me.</li>
     <li> I declare that I agree to accept any punishment pronounced by the disciplinary board of the National College of Education and the Ministry of Education if I am found and proven to have been engaged in any action against the code of conduct of National Colleges of Education.</li>
-
-</ol>
-    <p style="text-align:justify">
-    
-    
-    </p>
+  </ol>
+  &nbsp;
+    <div class="form-group">
+      <h2 style="text-align:center">අයදුම්කරුගේ ප්‍රකාශය</h2>
+    </div>
+  <ol style="line-height:25px; text-align:justify">
+    <li>මා විසින් ගැසට් නිවේදනය ඉතා හොඳින් කියවා අවබෝධ කරගෙන, 10.0 ඡේදයෙහි සඳහන් පරිදි අයදුම් පත යොමු කර ඇති බව මම දනිමි.</li>
+    <li>මා විසින් අයදුම් කර ඇති පාඨමාලා සඳහා ගැසට් නිවේදනයේ සඳහන් කර ඇති සුදුසුකම් සපුරා ඇති බව සහතික කරමි.</li>
+    <li>මා විසින් අයදුම්පතේ සපයා ඇති තොරතුරු සත්‍ය හා නිවැරදි බව සහතික කරමි.යම් හෙයකින් මවිසින් සපයා ඇති තොරතුරු අසත්‍ය හෝ වැරදි බව හෝ ගැසට් නිවේදනයට පටහැනි බව හෙළි වුවහොත් මා තෝරාගැනීමට නුසුදුස්සෙකු වන බවත්, තෝරාගැනීමෙන් පසුව සපයා ඇති තොරතුරු අසත්‍ය හෝ වැරදි බව හෝ ගැසට් නිවේදනයට පටහැනි බව හෝ හෙළි වුවහොත් මාගේ තෝරාගැනීම අවලංගු වන බවත් මම දනිමි.</li>
+    <li>මා විශ්වවිද්‍යාලයක/විශ්වවිද්‍යාල විද්‍යායතනයක ලියාපදිංචී වී නොමැති බවත් ඉහත කී ආයතනයක මේ වන විට අදාළ පාඨමාලාවක් හදාරමින් නොසිටින බවත් ගැසට් නිවේදනයේ 8.4 ඡේදය උල්ලංඝනය වන පරිදි කටයුතු කර ඇති බව හෝ කරනු ලබන බව හෝ සනාථ වුවහොත් පාඨමාලාවෙන් මා ඉවත් කරනු ලැබීමට එකඟ වන බවත් සහතික කරමි.</li>
+    <li>මා විසින් විද්‍යාපීඨ විනය නීතී රීතී මාලාවට පටහැනි යම් ක්‍රියාවක නිරත වී ඒ බව සනාථ වුවහොත් ඒ සම්බන්ධව විද්‍යාපීඨ විනය මණ්ඩලය හා අධ්‍යාපන අමාත්‍යංශය විසින් ගනු ලබන ඕනෑ ම දඩුවමකට මා යටත් වීමට එකඟ වනු ලබන බව ද සහතික කර ප්‍රකාශ කරමි.</li>
+  </ol>
+  &nbsp;
+  <div class="form-group">
+    <h2  style="text-align:center">விண்ணப்பதாரியின் உறுதியுரை</h2>
+  </div>
+  <ol style="line-height:25px; text-align:justify">
+    <li> என்னால் வர்த்தமானி அறிவித்தலை நன்றாக வாசித்து புரிந்து கொண்டு 10.0 பந்தியில் குறிப்பிடப்பட்டுள்ளதன்படி விண்ணப்பப்படிவம் அனுப்பி வைக்கப்பட்டு உள்ளதென்பதை நான் அறிந்துள்ளேன்.</li>
+    <li> என்னால் விண்ணப்பிக்கப்பட்டுள்ள பாடநெறிக்கு வர்த்தமானி அறிவித்தலின் குறிப்பிடப்பட்டுள்ள தகைமைகளை கொண்டுள்ளேன் என்பதையும் இத்தால் உறுதிப்படுத்துகின்றேன். </li>
+    <li> என்னால் விண்ணப்பப்படிவத்தில் வழங்கப்பட்டுள்ள தகவல்கள் சரியானதும் உண்மையானதும் என உறுதிசெய்கிறேன். ஏதேனும் வகையில் என்னால் வழங்கப்பட்ட தகவல்கள் உண்மைக்குப் புறம்பான அல்லது தவறானது அல்லது வர்த்தமானி அறிவித்தலுக்கு முரணானது எனத் தெரியவரும் பட்சத்தில் நான் தெரிவுசெய்யப்படுவதற்கு தகைமையற்றவர் என்றும்ääதெரிவுசெய்;ததன் பின்னர் என்னால்வழங்கப்பட்ட தகவல்கள் உண்மைக்குப் புறம்பானது அல்லது தவறானது அல்லது வர்த்தமானி அறிவித்தலுக்கு முரணானது எனத்தெரியவரும் பட்சத்தில் எனது தெரிவை இரத்துச் செய்ய முடியும் என்பதனையும் நான் அறிந்துள்ளேன்.</li>
+    <li> நான் பல்கலைக்கழக பிரவேசத்திற்கு விண்ணப்பிக்கவில்லை என்றும்ää பல்கலைக்கழகம் ஃபல்கலைக்கழக நிறுவகத்தில்; பதிவு செய்யவில்லை என்றும்ää மேலே கூறப்பட்ட நிறுவனம் ஒன்றிலேனும் தற்போது உரிய பாடநெறியைத் தொடரவில்லையென்றும்ää வர்த்தமானி அறிவித்தலின் 8.4 பந்தியை மீறும் வகையிலான செயற்பாடுகளில் ஈடுபட்டிருப்பின் அல்லது அவ்வாறு ஈடுபட்டமை தொடர்பாக உறுதிப்படுத்தப்படின் பாடநெறியிலிருந்து நான் விலக்கப்படுவதை ஒத்துக்கொள்வதாகவும் இத்தால் உறுதிசெய்கிறேன்.</li>
+    <li> என்னால் கல்லூரியின் ஒழுக்காற்று விதிமுறைகளுக்கு முரணான ஏதெனும் செயற்பாடுகளில் ஈடுபட்டிருப்பின் அல்லது அவ்வாறு ஈடுபட்டமை உறுதிப்படுத்தப்பட்டிருப்பின் அவை தொடர்பாக கல்லூரியின் ஒழுக்கக் குழு மற்றும் கல்வி அமைச்சினால் எடுக்கப்படும் எந்தவொரு தண்டனைக்கும் நான் உட்படுவதற்கு ஒத்துக்கொள்வதாகவும் இத்தால் சான்றுப்படுத்தி உறுதிசெய்கின்றேன்.</li>  
+  </ol>
     <br>
    
     <div class="form-group row">
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,25 +5,25 @@
     &nbsp;
 </div>
 <div class="row">
-    <div class="col-sm-3">
+    <div class="col-sm-2">
     </div>
-    <div class="col-sm-3">
+    <div class="col-sm-4">
         <div class="form-group"> 
             <div style="text-align: left;">
-                <a href="prirvenaal" class="btn btn-primary btn-lg" style="width: 100%;">Clergy<br>පූජ්‍ය/ පූජක</a>
+                <a href="prirvenaal" class="btn btn-primary btn-lg" style="width: 100%; padding-bottom:40px;">Clergy<br>පූජ්‍ය/ පූජක<br>மதகுருமார்கள் /வணக்கத்துக்குரியவர்கள்</a>
             </div>      
         </div>
     </div>
 <!-- </div>
 <div class="row"> -->
     <?php// var_dump($_SESSION); ?>
-    <div class="col-sm-3">   
+    <div class="col-sm-4">   
         <div class="form-group">  
             <div style="text-align :right;">
-                <a href="alresults" class="btn btn-primary btn-lg" style="width: 100%;">Non Clergy<br>පූජ්‍ය/ පූජක නොවන</a>
+                <a href="alresults" class="btn btn-primary btn-lg" style="width: 100%; padding:10px;">Non Clergy<br>පූජ්‍ය/ පූජක නොවන<br>மதகுருமார்கள் /வணக்கத்துக்குரியவர்கள் அல்லாதவர்கள</a>
             </div> 
         </div>
     </div>
-    <div class="col-sm-3">
+    <div class="col-sm-2">
     </div>
 </div>
