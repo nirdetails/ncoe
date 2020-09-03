8ef7bd58c718f989706d36a2081f8c6fd7e258c8
HarindaVithana <harindavithana@gmail.com>
2020-09-03 14:24:18 +0530

    Update AL.php

--- application/views/pages/AL.php
+++ application/views/pages/AL.php
@@ -1,25 +1,34 @@
 <h2><?= $title; ?></h2>
-<?php // echo validation_errors(); ?>
-<?php echo form_open_multipart('alresults/checkalres'); ?>
+<span style="color: red;"><?php echo validation_errors(); ?></span>
+<?php // echo form_open_multipart('alresults/checkalres'); ?>
+<form action="<?php echo site_url(); ?>alresults/checkalres" enctype="multipart/form-data" method="post" accept-charset="utf-8">
   <div class="form-group">
-    <label for="index">A\L index number:</label>
+    <label for="index">A\L index number: <span style="color:red">* </span></label>
     <input type="text" class="form-control" id="index" placeholder="Enter your A\L index number" name="index">
   </div>
   <div class="form-group">
+    <!-- <label for="git">Sat for the GIT exam: </label>
+    <select class="form-control" id="git" name="git" placeholder="Enter the GIT marks">
+      <option value="">SELECT</option>
+      <option value="yes">YES</option>
+      <option value="no">NO</option>
+    </select> -->
+  </div>
+  <!-- <div class="form-group">
     <label for="stream">Stream</label>
     <div class="form-group">
-    <select name="sel1" class="form-control" id="sel1">
-        <option id="maths" selected>STREAM</option>
-        <option id="maths">BIOLOGICAL SCIENCE STREAM - 1</option>
-        <option id="bio">PHYSICAL SCIENCE STREAM - 2</option>
-        <option id="commerce">COMMERCE - 3</option>
-        <option id="arts">ARTS - 4</option>
-        <option id="tech">TECHNOLOGY - 5</option>
-        <option id="comm">OTHER - 6</option>
+      <select name="sel1" class="form-control" id="sel1">
+        <option id="" selected>STREAM</option>
+        <option id="maths">MATHS</option>
+        <option id="bio">BIO SCIENCE</option>
+        <option id="commerce">COMMERCE</option>
+        <option id="arts">ARTS</option>
+        <option id="tech">TECHNOLOGY</option>
+        <option id="comm">COMMON</option>
       </select>
-    </div>
-    <br>
-  </div>
+    </div> -->
+    <!-- <br>
+  </div> -->
   
   <button type="submit" class="btn btn-info">Submit</button>
 
