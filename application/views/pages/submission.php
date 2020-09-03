de99b5e7176097c432f9d118c88f96d1faa6c241
HarindaVithana <harindavithana@gmail.com>
2020-09-03 14:34:27 +0530

    Update submission.php

--- application/views/pages/submission.php
+++ application/views/pages/submission.php
@@ -1,72 +1,78 @@
-<?php include "header.php" ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Personal Declaration</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-   -->
-<style>
-<?php include ('form/form.css')?>
-	/* body{
-		background: #aaa;
-	}
-	.container{
-		background: #fff;
-		padding: 10px;
-		margin-top: 10px;
-		border-radius: 10px;
-		max-width: 700px;
-  }
-  .button{
-
-  } */
-</style>
 <script>
 function myFunction() {
   alert("Your Application is submitted. Thank you!");
 }
 </script>
 
-</head>
-<body>
+<?php
+if($_SESSION['pdfdone'] == 1){
+  session_destroy();
+  redirect('/');
+}
+  // if (!isset($_SESSION["alindex"])){
+  //   redirect('/');
+  // }
+  // var_dump($_SESSION['pdfdone']);
 
-<div class="container">
-  <h2></h2>
-  <form action="/action_page.php">
-    
+?>
+  <h2><?= $title; ?></h2>
+<form action="<?php echo site_url(); ?>submission/submit" enctype="multipart/form-data" method="post" accept-charset="utf-8">
+  <?php //echo form_open_multipart('submission/submit'); ?>
+    <input type="hidden" value="<?php echo $_SESSION['alindex']; ?>" name="alindex">
+    <input type="hidden" value="<?php echo $_SESSION['olindex1']; ?>" name="olindex1">
+    <input type="hidden" value="<?php echo $_SESSION['olindex2']; ?>" name="olindex2">
+    <input type="hidden" value="<?php echo $_SESSION['olindex3']; ?>" name="olindex3">
+    <input type="hidden" value="<?php echo $_SESSION['sripada']; ?>" name="sripada">
+    <input type="hidden" value="<?php echo $_SESSION['sworker']; ?>" name="sworker">
 
+    <input type="hidden" value="<?php echo $_SESSION['fullname']; ?>" name="fullname">
+    <input type="hidden" value="<?php echo $_SESSION['namewithinitials']; ?>" name="namewithinitials">
+    <input type="hidden" value="<?php echo $_SESSION['dob']; ?>" name="dob">
+    <input type="hidden" value="<?php echo $_SESSION['addressline1']; ?>" name="addressline1">
+    <input type="hidden" value="<?php echo $_SESSION['addressline2']; ?>" name="addressline2">
+    <input type="hidden" value="<?php echo $_SESSION['addressline3']; ?>" name="addressline3">
+    <input type="hidden" value="<?php echo $_SESSION['addressline4']; ?>" name="addressline4">
+
+    <input type="hidden" value="<?php echo $_SESSION['district']; ?>" name="district">
+    <input type="hidden" value="<?php echo $_SESSION['nic']; ?>" name="nic">
+    <input type="hidden" value="<?php echo $_SESSION['gender']; ?>" name="gender">
+    <input type="hidden" value="<?php echo $_SESSION['title']; ?>" name="title">
+    <input type="hidden" value="<?php echo $_SESSION['ethnicity']; ?>" name="ethnicity">
+    <input type="hidden" value="<?php echo $_SESSION['mobile']; ?>" name="mobile">
+    <input type="hidden" value="<?php echo $_SESSION['home']; ?>" name="home">
+    <input type="hidden" value="<?php echo $_SESSION['email']; ?>" name="email">
+    <input type="hidden" value="<?php echo $_SESSION['pirivena']; ?>" name="pirivena">
+    <input type="hidden" value="<?php echo $_SESSION['git']; ?>" name="git">
+
+    <input type="hidden" value="<?php echo $_SESSION['course1']; ?>" name="course1">
+    <input type="hidden" value="<?php echo $_SESSION['course2']; ?>" name="course2">
+    <input type="hidden" value="<?php echo $_SESSION['course3']; ?>" name="course3">
+    <input type="hidden" value="<?php echo $_SESSION['pemarks1']; ?>" name="pemarks1">
+    <input type="hidden" value="<?php echo $_SESSION['category']; ?>" name="category">
+    <input type="hidden" value="<?php echo $_SESSION['gitmk']; ?>" name="gitmk">
+
+    <?php if($_SESSION['pemarks1'] != NULL){  ?>
+      <input type="hidden" value="<?php echo $_SESSION['pemarks1']; ?>" name="pemarks">
+    <?php } ?>
     <div class="row">
-        <div class="col-sm-6">
-            <div class="form-group"> 
-                <div style="text-align :left">
-                  <button style= "padding:20px;font-size:20px" type="submit" name="btnprv" class="btn btn-info"><span class="glyphicon glyphicon-arrow-left"></span> Recheck</button>
-                </div>      
-         </div>
-        </div>
-        <div class="col-sm-6">   
-            <div class="form-group">  
-               <div style="text-align :right">
-                  <button style= "padding:20px;font-size:20px" type="submit" name="btnnext" class="btn btn-info" onClick ="myFunction()"><span  class="glyphicon glyphicon-arrow-right"></span> Submit </button>
-               </div> 
-             </div>
-        </div>
+      <div class="col-sm-3">
+        &nbsp;
+      </div>
+      <div class="col-sm-3 form-group">
+        <a href="<?php echo site_url(); ?>pages/view/editpersonal" class="btn btn-info" style="width: 100%; height: 4em">Recheck Personal Details</a>
+      </div>
+      <div class="col-sm-3 form-group">
+        <a href="<?php echo site_url(); ?>courses/edit" class="btn btn-info" style="width: 100%; height: 4em">Recheck Choices</a>
+      </div>
+      <div class="col-sm-3">
+        &nbsp;
+      </div>
     </div>
-
-    <p style="text-align :center"><b>Download your application here:</b></br></br>
-        <a href="#" class="btn btn-info btn-lg">
-          <span class="glyphicon glyphicon-save-file"></span> Save File 
-        </a>
-      </p> 
-
-
-
+    <div class="row">
+      <div class="col-sm-4">&nbsp;</div>
+      <div class="col-sm-4">
+        <button style= "padding:20px;font-size:20px; width: 100%;" type="submit" name="btnprv" class="btn btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Submit</button>
+      </div>
+      <div class="col-sm-4">&nbsp;</div>
+    </div>
   </form>
-</div>
-
-</body>
-</html>
-<?php include "footer.php"?>
