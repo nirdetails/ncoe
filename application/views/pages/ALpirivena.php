203b0f3376742ac7efa91a3db62024e9c66d3cd0
HarindaVithana <harindavithana@gmail.com>
2020-09-03 14:27:31 +0530

    Update ALpirivena.php

--- application/views/pages/ALpirivena.php
+++ application/views/pages/ALpirivena.php
@@ -1,4 +1,4 @@
-<h2><?= $title; ?></h2>
+ <h2><?= $title; ?></h2>
 <?php // echo validation_errors(); ?>
 <?php //echo form_open_multipart('Alpirivena/checkalres'); ?>
 <form action="<?php echo site_url(); ?>Alpirivena/checkalres" enctype="multipart/form-data" method="post" accept-charset="utf-8">
