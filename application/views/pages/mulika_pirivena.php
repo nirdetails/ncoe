100a68612fbb4e70ffef604aa5c17295857371fe
HarindaVithana <harindavithana@gmail.com>
2021-10-04 12:30:05 +0530

    updated al.php

--- application/views/pages/mulika_pirivena.php
+++ application/views/pages/mulika_pirivena.php
@@ -45,11 +45,12 @@
 </div>
 </div>
 </div>
+
 <br>
 <footer class="footer">
-    <div class="container">
+    <!-- <div class="container"> -->
         <span><h4>©2020 Data Management Branch, Ministry of Education, Sri Lanka.</h4></span>
-    </div>
+    <!-- </div> -->
 </footer>
 <script src="<?php echo site_url(); ?>assets/js/ol.js"></script>
 
