d6a4a6c3a15a80a3c965957b46b55cd1690f534c
Dulanjana Bandara <dulanjanauop@gmail.com>
2021-11-12 13:19:01 +0530

    Centered the content on Download PDF page

--- application/views/pages/link.php
+++ application/views/pages/link.php
@@ -1,11 +1,8 @@
-<h5 > Your Application Is Submitted Successfully. Click The Below Link To Download The PDF Of Your Application .</h5>
+<div class="text-notice">
+	<h5>Your Application has been submitted successfully! Click the below link to download the PDF of your application.</h5>
+</div>
 
-<br>
-<div class="row">
-      <div class="col-sm-4">&nbsp;</div>
-      <div class="col-sm-4">
-    <a href="<?php echo site_url(); ?>pages/view/afterload" class="btn btn-info" style="width:50%; height: 3em; font-size: 1em; " target="_blank">Download PDF</a>
-    </div>
-      <div class="col-sm-4">&nbsp;</div>
-    </div>
+
+<div class="row button-submit">
+    <a href="<?php echo site_url(); ?>pages/view/afterload" class="btn btn-info" style="width: 15%; height: 3em; font-size: 20px; " target="_blank">Download PDF</a>
 </div>
