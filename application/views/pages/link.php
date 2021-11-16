bd84b3b870a77e80b2e608fb38b18205bbe766d6
HarindaVithana <harindavithana@gmail.com>
2021-11-16 16:21:48 +0530

    Updated submissions

--- application/views/pages/link.php
+++ application/views/pages/link.php
@@ -1,3 +1,8 @@
+<?php
+  if (!isset($_SESSION["alindex"])){
+    redirect('/');
+  }
+?>
 <div class="text-notice mb-5">
 	<h2>Your application has been submitted successfully! Click the below link to download the PDF of your application.</h2>
 </div>
@@ -5,4 +10,8 @@
 
 <div class="row button-submit">
     <a href="<?php echo site_url(); ?>pages/view/afterload" class="btn btn-info" style="width: 15%; height: 3em; font-size: 20px; " target="_blank">Download PDF</a>
+</div>
+<br>
+<div class="row button-submit">
+    <a href="<?php echo site_url(); ?>" class="btn btn-success" style="width: 15%; height: 3em; font-size: 20px; ">Home</a>
 </div>
