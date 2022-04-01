95ba2efe5dc82a6f1c071467a0640707efbf9f57
HarindaVithana <harindavithana@gmail.com>
2022-04-01 13:06:47 +0530

    Added dashboard functions

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -27,12 +27,109 @@
                         <tr> 
                             <td>District</td> 
                             <td>Count</td>
-                        </tr>
-                        <tr> 
-                            <td>Kurunegala</td> 
-                            <td>" . $this->db->count_all('stuents')->where('Resdistrict', '09'). "</td>
-                        </tr>
-                </table>";
+                        </tr>";
+            echo "<tr><td>Ampara</td><td>";
+                $this->db->where('Resdistrict','16');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Anuradhapura</td><td>";
+                $this->db->where('Resdistrict','20');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Badulla</td><td>";
+                $this->db->where('Resdistrict','22');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Batticaloa</td><td>";
+                $this->db->where('Resdistrict','15');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Colombo</td><td>";
+                $this->db->where('Resdistrict','01');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Galle</td><td>";
+                $this->db->where('Resdistrict','07');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Gampaha</td><td>";
+                $this->db->where('Resdistrict','02');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Hambantota</td><td>";
+                $this->db->where('Resdistrict','09');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Jaffna</td><td>";
+                $this->db->where('Resdistrict','10');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Kaluthara</td><td>";
+                $this->db->where('Resdistrict','03');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Kandy</td><td>";
+                $this->db->where('Resdistrict','04');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Kegalle</td><td>";
+                $this->db->where('Resdistrict','25');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Kilinochchi</td><td>";
+                $this->db->where('Resdistrict','11');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Kurunegala</td><td>";
+                $this->db->where('Resdistrict','18');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Mannar</td><td>";
+                $this->db->where('Resdistrict','12');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Matale</td><td>";
+                $this->db->where('Resdistrict','05');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Matara</td><td>";
+                $this->db->where('Resdistrict','08');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Monaragala</td><td>";
+                $this->db->where('Resdistrict','23');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Mullaitivu</td><td>";
+                $this->db->where('Resdistrict','14');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Nuwara Eliya</td><td>";
+                $this->db->where('Resdistrict','06');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Polonnaruwa</td><td>";
+                $this->db->where('Resdistrict','21');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Puttalam</td><td>";
+                $this->db->where('Resdistrict','19');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Ratnapura</td><td>";
+                $this->db->where('Resdistrict','24');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Trincomalee</td><td>";
+                $this->db->where('Resdistrict','17');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "<tr><td>Vavuniya</td><td>";
+                $this->db->where('Resdistrict','13');
+            echo $this->db->count_all_results('stuents');       
+            echo "</td>";
+            echo "</tr></table>";
+                // mysqli_fetch_array($query);
 ?>
             <br><br>
             <div class="row" style="padding-left: 10px;">
